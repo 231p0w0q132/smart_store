3bc94ec683e6cacbdb2f1ac01e25c5c0fd8580d1
231p0_w_0q132 <36323096+231p0w0q132@users.noreply.github.com>
2023-11-27 20:06:28 +0900

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,8 @@ app_name='main'
 urlpatterns= [
     path('',views.index,name='index'),
     path('ranged_weapon',views.ranged_weapon,name='ranged_weapon'),
+    path('combat_weapon',views.combat_weapon,name='combat_weapon'),
+    path('etc_weapon',views.etc_weapon,name='etc_weapon'),
+    path('all_weapon',views.all_weapon,name='all_weapon'),
 ]
 
