ccc999ea2cce5e1bde95fc7645a52fe4a4e80117
231p0_w_0q132 <36323096+231p0w0q132@users.noreply.github.com>
2023-11-27 20:03:37 +0900

    Update views.py

--- main/views.py
+++ main/views.py
@@ -6,6 +6,22 @@ def index(request):
     return render(request,"index.html")
 
 def ranged_weapon(request):
+    weapone=items.objects.filter(typ='원거리')
+    context={"items":weapone}
+    return render(request,"weapon_list.html",context)
+
+def combat_weapon(request):
+    weapone=items.objects.filter(typ='근접')
+    context={"items":weapone}
+    return render(request,"weapon_list.html",context)
+    
+def etc_weapon(request):
+    weapone=items.objects.filter(typ='기타')
+    context={"items":weapone}
+    return render(request,"weapon_list.html",context)
+    
+def all_weapon(request):
     weapone=items.objects.all()
     context={"items":weapone}
     return render(request,"weapon_list.html",context)
+    
